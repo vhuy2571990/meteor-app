5e2046a91f2165d0b8ec14412630a938bcb5421e
vhuy2571990 <vhuy257@gmail.com>
2017-10-11 15:32:23 +0700

    meteor tutorial code

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -1,22 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+angular.module('meteor-module', [
+  angularMeteor,
+  todosList.name,
+  'accounts.ui'
+]);
